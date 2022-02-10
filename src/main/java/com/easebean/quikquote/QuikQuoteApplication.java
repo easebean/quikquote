ec5cef73bb73090a2bbddd34a628638b61b31fac
liza-mathur <lisamathur0803@gmail.com>
2022-02-10 17:16:43 +0530

    initail commit

---
+++ src/main/java/com/easebean/quikquote/QuikQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.easebean.quikquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikQuoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easebean/quikquote/QuikQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easebean.quikquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuikQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
